782e603feb0b8f2be72f952594846314694f8d4a
Brad Teague <b_rad88@live.com>
2021-08-25 11:05:05 +0930

    test

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const pool = new Pool({
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASS,
   port: process.env.DB_PORT,
-  ssl: true,
+  ssl: { requireAuthorized: false },
 });
 
 const runQuery = async query => {
